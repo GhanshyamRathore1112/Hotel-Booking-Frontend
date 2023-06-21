88eb91a13ec5603a8f9c1829e85e733300966bf2
ghanshyam <202051072@iiitvadodara.ac.in>
2023-06-21 18:44:39 +0530

    updated home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
-
+import Featured from '../../components/featured/Featured'
+import "./Home.css"
+import PropertyList from '../../components/propertyList/propertyList'
+import FeaturedProperties from '../../components/featuredProperty/FeaturedProperties'
 const Home = () => {
   return (
     <div>
       <Navbar/>
-      <Header />   
+      <Header /> 
+      <div className='homeContainer'>
+       <Featured/> 
+       <h1 className="homeTitle">Browse Properties</h1> 
+       <PropertyList/>
+
+       <FeaturedProperties/>
+      </div>  
     </div>
   )
 }
