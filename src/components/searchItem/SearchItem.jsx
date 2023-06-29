f94d651ff0df8cbacff5d7b620f1061b2668d70a
rathore1112 <202051072@iiitvadodara.ac.in>
2023-06-29 18:01:42 +0530

    completed hotel page

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -29,7 +29,7 @@ const SearchItem = () => {
           <button>8.9</button>
         </div>
         <div className="siDetailTexts">
-          <span className="siPrice">$112</span>
+          <span className="siPrice">112/- Rs</span>
           <span className="siTaxOp">Includes taxes and fees</span>
           <button className="siCheckButton">See availability</button>
         </div>
