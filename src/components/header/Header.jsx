41ffc1fbe0c894fff8fe958e85677fd9a48805ee
rathore1112 <202051072@iiitvadodara.ac.in>
2023-09-22 10:03:56 +0530

    fix build errors

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,60 +1,67 @@
 import {
-    faBed,
-    faCalendarDays,
-    faCar,
-    faPerson,
-    faPlane,
-    faTaxi,
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./header.css"
+import "./header.css";
 import { DateRange } from "react-date-range";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
-import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
-const Header = ({type}) => {
-    const [destination, setDestination] = useState("");
-    const [openDate, setOpenDate] = useState(false);
-    const [date, setDate] = useState([
-        {
-          startDate: new Date(),
-          endDate: new Date(),
-          key: "selection",
-        },
-      ]);
-    
-      const [openOptions, setOpenOptions] = useState(false);
-      const [options, setOptions] = useState({
-          adult: 1,
-          children: 0,
-          room: 1,
-        });
-      const handleOption = (name, operation) => {
-            setOptions((prev) => {
-              return {
-                ...prev,
-                [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
-              };
-            });
-          };
+const Header = ({ type }) => {
+  const [destination, setDestination] = useState("");
+  const [openDate, setOpenDate] = useState(false);
+  const [dates, setDates] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
 
-        const navigate =useNavigate()
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
-        const handleSearch = () => {
-            navigate("/hotels", { state: { destination, date, options } });
-          };
 
+  const handleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
+  const { dispatch } = useContext(SearchContext);
+
+  const handleSearch = () => {
+    dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/hotels", { state: { destination, dates, options } });
+  };
 
   return (
     <div className="header">
-      <div className={
+      <div
+        className={
           type === "list" ? "headerContainer listMode" : "headerContainer"
-        }>
-      <div className="headerList">
+        }
+      >
+        <div className="headerList">
           <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
@@ -75,49 +82,54 @@ const Header = ({type}) => {
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport taxis</span>
           </div>
-      </div>
-      {type !== "list" && (
-          <>
-      <h1 className="headerTitle">Looking for a hotel</h1>
-      <p className="headerDesc">
-        Lorem ipsum
-      </p>
-      <button className="headerBtn">Sign In/Register</button>
-      <div className="headerSearch">
-        <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faBed} className="headerIcon"/>
-            <input 
-             type="text"
-             placeholder="whereare you going"
-             className="headerSearchInput"
-             onChange={e=>setDestination(e.target.value)}
-            />
         </div>
-        <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
-            <span onClick={() => setOpenDate(!openDate)} className="headerSearchText">
-            {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                  date[0].endDate,
+        {type !== "list" && (
+          <>
+            <h1 className="headerTitle">
+              A lifetime of discounts? It's Genius.
+            </h1>
+            <p className="headerDesc">
+              Get rewarded for your travels – unlock instant savings of 10% or
+              more with a free Lamabooking account
+            </p>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
+            <div className="headerSearch">
+              <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                <input
+                  type="text"
+                  placeholder="Where are you going?"
+                  className="headerSearchInput"
+                  onChange={(e) => setDestination(e.target.value)}
+                />
+              </div>
+              <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                <span
+                  onClick={() => setOpenDate(!openDate)}
+                  className="headerSearchText"
+                >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                  dates[0].endDate,
                   "MM/dd/yyyy"
-                )}`}
-            </span>
-            {openDate && (
+                )}`}</span>
+                {openDate && (
                   <DateRange
                     editableDateInputs={true}
-                    onChange={(item) => setDate([item.selection])}
+                    onChange={(item) => setDates([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={date}
+                    ranges={dates}
                     className="date"
                     minDate={new Date()}
                   />
                 )}
-        </div>
-        <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
-            <span onClick={() => setOpenOptions(!openOptions)} className="headerSearchText">
-            {`${options.adult} adult · ${options.children} children · ${options.room} room`}
-            </span>
-            {openOptions && (
+              </div>
+              <div className="headerSearchItem">
+                <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                <span
+                  onClick={() => setOpenOptions(!openOptions)}
+                  className="headerSearchText"
+                >{`${options.adult} adult · ${options.children} children · ${options.room} room`}</span>
+                {openOptions && (
                   <div className="options">
                     <div className="optionItem">
                       <span className="optionText">Adult</span>
@@ -184,16 +196,18 @@ const Header = ({type}) => {
                     </div>
                   </div>
                 )}
-        </div>
-        <div className="headerSearchItem">
-           <button className="headerBtn" onClick={handleSearch}></button>
-        </div>
-      </div>
-      </>
+              </div>
+              <div className="headerSearchItem">
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
